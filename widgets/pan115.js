e2e7c084c2afb49d5e26052190b02c77b13130fa
szyiwei <57577282+szyiwei@users.noreply.github.com>
2026-08-02 09:24:54 +0800

    Update pan115.js

--- widgets/pan115.js
+++ widgets/pan115.js
@@ -410,7 +410,6 @@ async function listFolder(cookie, cid, page) {
 
 // 修改后：使用搜索接口进行穿透，强制限定 type=4 (视频)
   var url = WEB_API_115 + "/files/search?cid=" + encodeURIComponent(cid)
-    + "&search_value=" + encodeURIComponent(".mkv|.mp4|.avi|.wmv")
     + "&type=4"
     + "&file_size=524288000"
     + "&offset=" + offset + "&limit=" + limit
